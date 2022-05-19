c4eede8dce0a75702d44ac5f410bf2c8c75c145c
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-19 13:08:26 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let num: number[] = new Array(7);
+let índice: number = 0;
+
+num[0] = 20;
+num[1] = 14;
+num[2] = 8;
+num[3] = 0;
+num[4] = 5;
+num[5] = 19;
+num[6] = 24;
+
+while (índice <= 6) {
+  console.log(`El número en la posición ${índice} es ${num[índice]}`);
+  índice++;
+}
